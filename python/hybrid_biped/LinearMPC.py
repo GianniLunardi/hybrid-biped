035fcbbb5af42aa39e814df21e9638465041bdbd
GianniLunardi <gianni.lunardi96@gmail.com>
2023-05-11 10:29:55 +0200

    minor changes
    - save the horizon data
    - include the varying CoP trajectory method directly inside the clas

--- python/hybrid_biped/LinearMPC.py
+++ python/hybrid_biped/LinearMPC.py
@@ -28,6 +28,8 @@ class LipmMPC:
         self.foot_steps_des = lw.manual_foot_placement(self.params.foot_step_0,
                                                        self.params.step_length,
                                                        self.params.nb_steps + 3)
+        self.horizon_data = []
+        self.delay = True
 
     def updateCurrentState(self, tau_hat, x_hat, y_hat):
         """ Update the initial state of the MPC step from the current values of the HS and TSID """
@@ -35,14 +37,21 @@ class LipmMPC:
         self.x_hat_k = x_hat
         self.y_hat_k = y_hat
 
+    def setTimeLeft(self, nb_left):
+        """ Set the expected time for the next step, remembering the different time steps btw hybrid LIP and MPC"""
+        self.nb_left = round(nb_left / self.params.ratio)
+
     def step(self):
+        horizon_data = {}
         # Rollout the HS dynamics to compute the time needed for the first foot step
         nb_left = rolloutLipmDynamics(self.x_hat_k, self.tau_hat_k, self.dt_mpc, self.params)
+        # print("iter: ", i,  "nb_left: ", nb_left)
 
         # Plan the CoP reference
-        self.Z_ref_k, fsteps = lw.varying_CoP_trajectory(self.foot_steps_des, self.horizon,
-                                                         self.nb_steps_per_T, nb_left)
-        self.foot_steps_des = np.copy(fsteps)
+        # self.Z_ref_k, fsteps = lw.varying_CoP_trajectory(self.foot_steps_des, self.horizon,
+        #                                                  self.nb_steps_per_T, nb_left)
+        # self.foot_steps_des = np.copy(fsteps)
+        self.Z_ref_k = self.varying_CoP_trajectory(nb_left)
 
         Q_k, p_k = lw.compute_objective_terms(self.alpha, self.beta, self.gamma,
                                               self.params.T, self.nb_steps_per_T, self.horizon,
@@ -58,8 +67,35 @@ class LipmMPC:
         X_k, Y_k = lw.compute_recursive_dynamics(self.P_ps, self.P_vs, self.P_pu, self.P_vu,
                                                  self.horizon, self.x_hat_k, self.y_hat_k, current_U)
 
+        horizon_data['zmp_reference'] = self.Z_ref_k
+        horizon_data['X_k'] = X_k
+        horizon_data['Y_k'] = Y_k
+        horizon_data['Z_x_k'] = current_U[:self.horizon]
+        horizon_data['Z_y_k'] = current_U[self.horizon:]
+
         # Save the CoM and CoP (only y directions are interesting
         self.y_next = Y_k[0,:]
         self.U_y_current = current_U[self.horizon]
         self.Z_y = self.Z_ref_k[0,1]
 
+        self.horizon_data.append(horizon_data)
+
+    def varying_CoP_trajectory(self, nb_left):
+        """ Compute the CoP reference trajectory for the MPC horizon """
+        Z_ref = np.zeros((self.horizon, 2))
+        if nb_left < 2:
+            if self.delay:
+                self.foot_steps_des = self.foot_steps_des[1:,:]
+            Z_ref[:self.nb_steps_per_T,:] = self.foot_steps_des[0,:]
+            Z_ref[self.nb_steps_per_T:,:] = self.foot_steps_des[1,:]
+            self.delay = False
+        elif nb_left + self.nb_steps_per_T < self.horizon:
+            Z_ref[:nb_left,:] = self.foot_steps_des[0,:]
+            Z_ref[nb_left:nb_left + self.nb_steps_per_T,:] = self.foot_steps_des[1,:]
+            Z_ref[nb_left + self.nb_steps_per_T:,:] = self.foot_steps_des[2,:]
+            self.delay = True
+        else:
+            Z_ref[:nb_left,:] = self.foot_steps_des[0,:]
+            Z_ref[nb_left:,:] = self.foot_steps_des[1,:]
+            self.delay = True
+        return Z_ref
