c27dd47655dac2d8fe6501a5e64682a9d1aeaf61
GianniLunardi <gianni.lunardi96@gmail.com>
2023-05-11 10:31:41 +0200

    changes for the interpolation class
    - FIX a wrong variable (g)
    - add the foot trajectory computations without rescaling

--- python/hybrid_biped/LIPM_to_WBD.py
+++ python/hybrid_biped/LIPM_to_WBD.py
@@ -7,13 +7,15 @@ class LipmToWbc:
 
     def __init__(self, params):
         self.dt = params.dt
-        self.g = params.omega
+        self.g = params.g
         self.h_com = params.h_com
         self.omega = params.omega
         self.r_bar = params.r_bar
         self.T = params.T
         self.foot_h = params.step_height
         self.params = params
+        # Default rescaling = 1
+        self.alpha = 1
 
     def computeCoMAcceleration(self, c_p, u):
         return self.g/self.h_com * (c_p - u)
@@ -38,11 +40,12 @@ class LipmToWbc:
         poly_ddot = (6*d*t + 12*e*t**2 + 20*f*t**3 + 30*g*t**4) * self.alpha**2
         return np.array([poly, poly_dot, poly_ddot]).T
 
-    def footTrajectoryFromHS(self, foot_init, foot_fin, foot_h = None):
+    def footTrajectoryFromHS(self, foot_init, foot_fin, foot_h = None, rescaling = True):
         if foot_h is None:
             foot_h = self.foot_h
         foot_pos = np.zeros((3,3))
-        self.alpha = self.T /(self.t + self.t_left)
+        if rescaling:
+            self.alpha = self.T /(self.t + self.t_left)
         foot_pos[:2,:] = self.fifthOrderPolynomial(self.alpha * self.t, foot_init, foot_fin)
         foot_pos[2,:] = self.sixthOrderPolynomial(self.alpha * self.t, foot_h)
         return foot_pos
@@ -51,9 +54,10 @@ class LipmToWbc:
         A_d, B_d = lw.discrete_LIP_dynamics((j+1) * self.dt, self.omega)
         return A_d.dot(y) + B_d * u_y
 
-    def set_time(self, t_hs, x, tau):
+    def set_time(self, t_hs, x = None, tau = None):
         self.t = t_hs
-        self.t_left = self.dt * rolloutLipmDynamics(x, tau, self.dt, self.params)
+        if x is not None and tau is not None:
+            self.t_left = self.dt * rolloutLipmDynamics(x, tau, self.dt, self.params)
 
     def set_time_offline(self, t_hs, t_pred):
         self.t = t_hs
