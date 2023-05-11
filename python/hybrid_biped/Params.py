bf98b01d0832c6bbe038da167cb229042f237c53
GianniLunardi <gianni.lunardi96@gmail.com>
2023-05-11 10:18:00 +0200

    changes on the parameters:
    - increase CoM height
    - reduced step length
    - trial feet coordinates for the example of inverse kinematics
    - correct URDF
    - torso task parameters

--- python/hybrid_biped/Params.py
+++ python/hybrid_biped/Params.py
@@ -3,18 +3,18 @@ import scipy.io
 
 class BipedParams:
 
-    def __init__(self, filename = None):
-        self.h_com = 0.66  #0.58
+    def __init__(self):
+        self.h_com = 0.58
         self.g = 9.81
         self.omega = np.sqrt(self.g / self.h_com)
 
         self.nb_steps = 5
-        self.T = 1.2
+        self.T = 1.2    #1.2
         self.dt = 1e-3
         self.dt_mpc = 3 * 1e-2
         self.ratio = int(self.dt_mpc / self.dt)
 
-        self.r_bar = 0.15
+        self.r_bar = 0.025 #0.15
         self.v_bar = -self.omega * self.r_bar * (1 + np.exp(self.omega * self.T)) / (1 - np.exp(self.omega * self.T))
 
         foot_scaling = 1.
@@ -30,41 +30,53 @@ class BipedParams:
         data = scipy.io.loadmat('/home/glunardi/devel/src/hybrid-biped/data/data.mat')
         self.K = data['K']
 
-        # ICs
-        self.foot_step_0 = np.array([0., -0.08])
-        self.tau_hat_0 = data['x0'][-1].item()
-        self.x_hat_0 = data['x0'][:-1].T[0]
-        self.y_hat_0 = np.array([-0.04, 0.0])
-
         self.step_length = self.r_bar * 2
-        self.step_width = 2 * np.absolute(self.y_hat_0[0])
+        self.step_width = 0.192
         self.step_height = 0.05
 
+        # ICs
+        self.foot_step_0 = np.array([0., -self.step_width/2])
+        self.tau_hat_0 = data['x0'][-1].item()
+        self.x_hat_0 = data['x0'][:-1].T[0]
+        self.y_hat_0 = np.array([-0.09, 0.1])
+        self.ref_0 = data['ref0'].T[0]
+
+        # Feet trial coordinates
+        self.x_LF_trial = np.array([0., self.step_width/2, 0.07])
+        self.x_RF_trial = np.array([-0.0, -self.step_width/2, 0.12])
+
         # Weights
-        self.alpha = 1e2
+        self.alpha = 1e1
         self.beta = 0
-        self.gamma = 1e-1
+        self.gamma = 1e-2
 
         # Configuration for TSID
-        self.path = '/opt/openrobots/share/example-robot-data/robots/romeo_description'
+        # self.path = '/opt/openrobots/share/example-robot-data/robots/romeo_description'
+        # self.urdf = self.path + '/urdf/romeo.urdf'
+        # self.srdf = self.path + '/srdf/romeo.srdf'
+
+        self.path = '/home/glunardi/devel/src/temp/tsid/models/romeo'
         self.urdf = self.path + '/urdf/romeo.urdf'
-        self.srdf = self.path + '/srdf/romeo.srdf'
+        self.srdf = self.path + '/srdf/romeo_collision.srdf'
+
         self.nv = 37
         self.lz = 0.07  # foot sole height with respect to ankle joint
-        self.mu = 0.3  # friction coefficient
+        self.mu = 0.7  # friction coefficient
         self.fMin = 0.0  # minimum normal force
         self.fMax = 1e6  # maximum normal force
         self.rf_frame_name = "RAnkleRoll"  # right foot frame name
         self.lf_frame_name = "LAnkleRoll"  # left foot frame name
-        self.contactNormal = np.matrix([0., 0., 1.]).T  # direction of the normal to the contact surface
+        self.torso_frame_name = "torso"  # torso frame name
+        self.contactNormal = np.array([0., 0., 1.])  # direction of the normal to the contact surface
         self.contact_phase_0 = 'right'
 
-        self.T_pre = 1.5  # simulation time before starting to walk
+        # self.T_pre = 1.5  # simulation time before starting to walk
         self.T_post = 1.5  # simulation time after walking
 
         self.w_com = 1.0  # weight of center of mass task
         self.w_cop = 0.0  # weight of center of pressure task
-        self.w_am = 1e-4  # weight of angular momentum task
+        self.w_am = 0#1e-4  # weight of angular momentum task
+        self.w_torso = 1e-2  # weight of torso orientation task
         self.w_foot = 1e0  # weight of the foot motion task
         self.w_contact = 1e2  # weight of the foot in contact
         self.w_posture = 1e-4  # weight of joint posture task
@@ -77,11 +89,13 @@ class BipedParams:
 
         self.kp_contact = 10.0  # proportional gain of contact constraint
         self.kp_foot = 10.0  # proportional gain of contact constraint
-        self.kp_com = 3.0  # proportional gain of center of mass task
+        self.kp_com = 10.0  # proportional gain of center of mass task
         self.kp_am = 10.0  # proportional gain of angular momentum task
+        self.kp_torso = 10.0  # proportional gain of torso orientation task
         self.kp_posture = 1.0  # proportional gain of joint posture task
         self.gain_vector = self.kp_posture * np.ones(self.nv - 6)
         self.masks_posture = np.ones(self.nv - 6)
+        self.mask_torso = np.array([0, 0, 0, 1, 1, 1])
 
         # Configuration for viewer
         self.PRINT_N = 500  # print every PRINT_N time steps
